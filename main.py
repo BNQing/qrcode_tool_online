a6c367d21e298fe6438f35188667f03015e8f722
BNQing <756229372@qq.com>
2019-07-02 11:06:09 +0800

    优化二维码在线生成器

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import flask
 import qrcode
+import io
 
 app= flask.Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    # #第一步：获取要生成二维码的数据
-    # data = flask.request.args.get("data")
-
-    # #第二步：生成二维码图像
-    # img = qrcode.make(data)
-    # img.save(r"C:\Users\Administrator\Desktop\day6\代码\qrcode_tool_online\static\qr.png")
-
-    # #第三步：在页面上显示二维码图片
-    # return '<img src="/static/qr.png" />'
-
     return flask.render_template('qr_tool.html')
 
-@app.route('/qr',methods=["POST"])
+@app.route("/qr")
 def qr():
     # #第一步：获取要生成二维码的数据
-    data = flask.request.form.get("data")
+    data = flask.request.args.get("data")
+
 
     # #第二步：生成二维码图像
     img = qrcode.make(data)
-    img.save(r"C:\Users\Administrator\Desktop\day6\代码\qrcode_tool_online\static\qr.png")
+    bi = io.BytesIO()  #创建一个BytesIO对象，用于在内存中储存二维码图像数据
+    img.save(bi,"png") #调用img对象的save方法将二维码图像数据以png编码格式写入对象管理的内存空间
+    bi.seek(0)  #将bi对象内部的位置指针移动到图像数据的起始位置
 
-    # #第三步：在页面上显示二维码图片
-    return '<img src="/static/qr.png" />'
+    # #第三步：返回二维码图像数据
+    return flask.send_file(bi,"image/png")
 
 if __name__ == '__main__':
     app.run(debug=True)
